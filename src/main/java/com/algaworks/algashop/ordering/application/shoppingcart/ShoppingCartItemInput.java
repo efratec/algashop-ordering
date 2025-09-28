545659d78510b0a0f0280f126ebdfcce3e61c3e5
Efraim Tenorio <efratec20.tenorio@gmail.com>
2025-09-28 12:07:47 -0300

    Desafio - Application Service para gestão de Shopping Cart

---
+++ src/main/java/com/algaworks/algashop/ordering/application/shoppingcart/ShoppingCartItemInput.java
@@ -0,0 +1,4 @@
+package com.algaworks.algashop.ordering.application.shoppingcart;
+
+public class ShoppingCartItemInput {
+}
---
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/shoppingcart/ShoppingCartNotFoundException.java
@@ -0,0 +1,4 @@
+package com.algaworks.algashop.ordering.domain.model.shoppingcart;
+
+public class ShoppingCartNotFoundException {
+}
